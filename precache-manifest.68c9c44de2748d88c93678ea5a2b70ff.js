5577016317502edcdb26e082d96063775b56c3e0
Karim Mokhtar <karimmokhtar28@gmail.com>
2022-01-11 21:20:00 +0200

    Updates

---
+++ precache-manifest.68c9c44de2748d88c93678ea5a2b70ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427a5f33464fe96f936b9b383b7feb77",
+    "url": "/posts/index.html"
+  },
+  {
+    "revision": "c26110f97c8f85e0d5b4",
+    "url": "/posts/static/css/2.f3ac9842.chunk.css"
+  },
+  {
+    "revision": "50a98471b7b7dba0b572",
+    "url": "/posts/static/css/main.da277c8c.chunk.css"
+  },
+  {
+    "revision": "c26110f97c8f85e0d5b4",
+    "url": "/posts/static/js/2.a9006308.chunk.js"
+  },
+  {
+    "revision": "87affe29fb7aba4711b7ec369493bc03",
+    "url": "/posts/static/js/2.a9006308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26131a319fd2455c5a0",
+    "url": "/posts/static/js/3.090c6a06.chunk.js"
+  },
+  {
+    "revision": "50a98471b7b7dba0b572",
+    "url": "/posts/static/js/main.9e516bd4.chunk.js"
+  },
+  {
+    "revision": "ee4ef0a9ba288ce6662e",
+    "url": "/posts/static/js/runtime-main.c2551467.js"
+  }
+]);
